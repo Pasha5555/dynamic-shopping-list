d8e6999d744052491d30d63ce7cadb9d32941f69
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2024-12-10 21:42:44 +0200

    redeploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BASE_URL = 'https://dynamic-shopping-list.vercel.app';
 export const PRODUCT_OPTIONS = ['Fruits', 'Dairy', 'Vegetables'];
